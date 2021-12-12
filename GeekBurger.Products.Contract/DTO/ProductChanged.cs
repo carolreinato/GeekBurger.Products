acee1c764e364f3116cea570993a97d7d044bbbe
Caroline Reinato <cr.reinato@gmail.com>
2021-12-12 12:25:14 -0300

    Add project files.

---
+++ GeekBurger.Products.Contract/DTO/ProductChanged.cs
@@ -0,0 +1,10 @@
+﻿using GeekBurger.Products.Contract.Enum;
+
+namespace GeekBurger.Products.Contract.DTO
+{
+    public class ProductChanged
+    {
+        public ProductState State { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ GeekBurger.Products.Contract/Enum/ProductState.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekBurger.Products.Contract.Enum
+{
+    public enum ProductState
+    {
+        Deleted = 2,
+        Modified = 3,
+        Added = 4
+    }
+}
